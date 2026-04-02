e502017f0c512e743618df157cfb1e8be1910885
Sankar <veerd630478@gmail.com>
2026-04-02 17:26:34 +0530

    From this commint changing from tavily to langchain-tavily

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+tavily = TavilyClient()
+
+@tool
+def search(query: str) -> str :
+    """
+    Tool that searches over the internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"searching for {query}")
+    return tavily.search(query= query)
+
+llm = ChatOpenAI(model= "gpt-5")
+tools = [search]
+agent = create_agent(model= llm, tools= tools)
+
+
 def main():
     print("Hello from langchain-course!")
-
+    result = agent.invoke({"messages": HumanMessage(content= "What is the weather in Tokyo")})
+    print(result)
 
 if __name__ == "__main__":
     main()
